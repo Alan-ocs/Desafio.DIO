f1c14db4a97c5c67d0128d4880f569d5d681c1b1
Alan Silva <alan.ocs@gmail.com>
2022-08-01 00:49:37 +0000

    Updates with variables

--- Linux Experience/IaC/iac1.sh
+++ Linux Experience/IaC/iac1.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 
+grupo_01="GRP_ADM"
+grupo_02="GRP_VEN"
+grupo_03="GRP_SEC"
+
 echo "Criando diretórios..."
 
 mkdir /publico
-mkdir /adm
-mkdir /ven
-mkdir /sec
+mkdir /$grupo_01
+mkdir /$grupo_02
+mkdir /$grupo_03
 
 echo "Criando grupos de usuários..."
 
-groupadd GRP_ADM
-groupadd GRP_VEN
-groupadd GRP_SEC
+groupadd $grupo_01
+groupadd $grupo_02
+groupadd $grupo_03
 
 echo "Criando usuários..."
 
-useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
-useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_ADM
+useradd carlos -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G $grupo_01
+useradd maria -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G $grupo_01
+useradd joao -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G $grupo_01
 
-useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
-useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_VEN
+useradd debora -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G $grupo_02
+useradd sebastiana -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G $grupo_02
+useradd roberto -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G $grupo_02
 
-useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
-useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G GRP_SEC
+useradd josefina -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G $grupo_03
+useradd amanda -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G $grupo_03
+useradd rogerio -m -s /bin/bash -p $(openssl passwd -crypt Senha123) -G $grupo_03
 
 echo "Especificando permissões dos diretórios...."
 
-chown root:GRP_ADM /adm
-chown root:GRP_VEN /ven
-chown root:GRP_SEC /sec
+chown root:$grupo_01 /$grupo_01
+chown root:$grupo_02 /$grupo_02
+chown root:$grupo_03 /$grupo_03
 
-chmod 770 /adm
-chmod 770 /ven
-chmod 770 /sec
+chmod 770 /$grupo_01
+chmod 770 /$grupo_02
+chmod 770 /$grupo_03
 chmod 777 /publico
 
 echo "Fim....."
